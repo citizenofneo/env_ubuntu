2cde26dd5adfef6430969be1a74ec18710f5e03f
Bob Braun <bobbraunjedi@gmail.com>
2019-11-01 12:59:04 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,8 +3,9 @@ sudo apt update
 sudo apt install git
 sudo apt install curl
 sudo apt install policykit-1
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo npm i -g pm2
 sudo apt install software-properties-common
 sudo apt-add-repository ppa:nginx/stable
 sudo apt update
