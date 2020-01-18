7a60948bcd1038ae7a1906d4936b09aa96f57a8b
Bob Braun <bobbraunjedi@gmail.com>
2020-01-18 18:41:08 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,16 +1,16 @@
-apt-get install sudo
+apt-get install sudo -y
 sudo apt update
 apt upgrade 
-sudo apt install git
-sudo apt install curl
-sudo apt install policykit-1
+sudo apt install git -y
+sudo apt install curl -y
+sudo apt install policykit-1 -y
 sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs -y
 sudo npm i -g pm2
-sudo apt install software-properties-common
-sudo apt-add-repository ppa:nginx/stable
+sudo apt install software-properties-common -y
+sudo apt-add-repository ppa:nginx/stable -y
 # 2й раз запустить то что ниже 
 sudo apt update
-sudo apt install nginx
+sudo apt install nginx -y
 sudo systemctl start nginx
 pm2 startup
