354eac63e6575ce2b3fb20753daacbe89e6e43bc
Bob Braun <bobbraunjedi@gmail.com>
2020-01-30 16:51:56 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,17 +1,17 @@
-apt-get install sudo -y
-sudo apt update -y
-apt upgrade -y
-sudo apt install git -y
-sudo apt install curl -y
-sudo apt install policykit-1 -y
-sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs -y
-sudo npm i -g pm2 -y
-sudo apt install software-properties-common -y
-sudo apt-add-repository ppa:nginx/stable -y
-# 2й раз запустить то что ниже 
-sudo apt update -y
-sudo apt install nginx -y
-sudo apt install fail2ban -y
-sudo systemctl start nginx
+apt-get install sudo -y && 
+sudo apt update -y && 
+apt upgrade -y && 
+sudo apt install git -y && 
+sudo apt install curl -y && 
+sudo apt install policykit-1 -y && 
+sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && 
+sudo apt-get install -y nodejs -y && 
+sudo npm i -g pm2 -y && 
+sudo apt install software-properties-common -y && 
+sudo apt-add-repository ppa:nginx/stable -y && 
+sudo apt update -y && 
+sudo apt install nginx -y && 
+sudo apt install fail2ban -y && 
+sudo systemctl start nginx && 
 pm2 startup
+echo Done
