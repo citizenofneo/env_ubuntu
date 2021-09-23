cea5d660bc488b33912c58ad4375f27f0330b0bc
Citizen <80706241+citizenofneo@users.noreply.github.com>
2021-09-24 00:39:33 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -10,13 +10,13 @@ sudo npm i -g pm2 -y &&
 
 #rm apache
 sudo service apache2 stop -y &&
-sudo apt-get purge apache2* -y
-sudo apt-get autoremove -y
+sudo apt-get purge apache2* -y &&
+sudo apt-get autoremove -y &&
 sudo rm -r -f /*/apache2* -y
 
-sudo apt install software-properties-common -y && 
-sudo apt-add-repository ppa:nginx/stable -y && 
-sudo apt update -y && 
+#sudo apt install software-properties-common -y && 
+#sudo apt-add-repository ppa:nginx/stable -y && 
+#sudo apt update -y && 
 sudo apt install nginx -y && 
 sudo apt install fail2ban -y &&
 sudo rm /etc/nginx/sites-enabled/default &&
